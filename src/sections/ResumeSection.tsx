6480f1d3d49edec972acc3ab447a6c50e457a406
RitikPuranik <ritikpuranik28@gmail.com>
2026-07-14 23:29:17 +0530

    made some changes

--- src/sections/ResumeSection.tsx
+++ src/sections/ResumeSection.tsx
@@ -31,7 +31,7 @@ const education = [
 
 const experience = [
   {
-    years: 'Jan 2026 – Jun 2026',
+    years: 'January 2026 – June 2026',
     title: 'Backend Developer Intern',
     detail: 'CodeWaveSolution, Bhopal',
     description: '6 months internship to make and manage the website of CodeWaveSolution, an education platform.',
